466ef0d5a700b05df2eab60ccc50927efbfe3f59
ic-cw <76678425+ic-cw@users.noreply.github.com>
2021-11-05 20:19:24 +0900

    feat : 음계 문제 해결

---
+++ src/com/algorithm/bakjoon/Bakjoon2920.java
@@ -0,0 +1,26 @@
+package com.algorithm.bakjoon;
+
+import java.util.Scanner;
+
+public class Bakjoon2920 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[] arr = new int[8];
+        for (int i = 0; i < 8; i++) {
+            arr[i] = scanner.nextInt();
+        }
+
+        String result = "";
+        for (int i = 0; i < arr.length - 1; i++) {
+            if (arr[i + 1] == arr[i] + 1) {
+                result = "ascending";
+            } else if (arr[i + 1] == arr[i] - 1) {
+                result = "descending";
+            } else {
+                result = "mixed";
+                break;
+            }
+        }
+        System.out.println(result);
+    }
+}
