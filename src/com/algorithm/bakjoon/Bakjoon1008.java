6274b3a3d9f95df3cc1388ce57481cf95a320a1a
ic-cw <76678425+ic-cw@users.noreply.github.com>
2021-11-05 16:08:18 +0900

    feat : A / B 구현

---
+++ src/com/algorithm/bakjoon/Bakjoon1008.java
@@ -0,0 +1,16 @@
+package com.algorithm.bakjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Bakjoon1008 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        Double a = Double.parseDouble(st.nextToken());
+        Double b = Double.parseDouble(st.nextToken());
+        System.out.println(a / b);
+    }
+}
